34e88d755722edfe265e2010f1e2a4627bf029b0
sabr5840 <sabr5840@stud.kea.dk>
2023-08-31 14:59:49 +0200

    Initial commit

---
+++ src/test/java/com/example/githubactions22/GitHubActions22ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubactions22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubActions22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
