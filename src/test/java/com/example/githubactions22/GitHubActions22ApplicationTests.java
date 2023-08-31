cf288939d0a2b3e4bcbae64f78d0b37410600944
sabr5840 <sabr5840@stud.kea.dk>
2023-08-31 15:00:04 +0200

    github action

--- src/test/java/com/example/githubactions22/GitHubActions22ApplicationTests.java
+++ src/test/java/com/example/githubactions22/GitHubActions22ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GitHubActions22ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
